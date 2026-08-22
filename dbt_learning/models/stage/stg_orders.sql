831eee26593fba821c80b39dd68560e608c53537
unknown <shihab.ahmed.aljassasi@gmail.com>
2026-08-22 10:02:57 +0400

    done with week3 but gradefile in fact_order because i don't have fact_order.sql file

--- dbt_learning/models/stage/stg_orders.sql
+++ dbt_learning/models/stage/stg_orders.sql
@@ -1,9 +1,12 @@
 with source as (
+
     select *
     from {{ ref('raw_orders') }}
+
 ),
 
 cleaned as (
+
     select
         order_id::integer as order_id,
         trim(customer_id)::text as customer_id,
@@ -11,9 +14,38 @@ cleaned as (
         order_date::date as order_date,
         lower(trim(status))::text as order_status,
         coalesce(shipping_fee, 0)::numeric(12,2) as shipping_fee,
-	upper(trim(currency))::text as currency
+        upper(trim(currency))::text as currency
+
     from source
+
+),
+
+deduped as (
+
+    select *
+
+    from (
+        select *,
+            row_number() over (
+                partition by order_id
+                order by order_date desc
+            ) as rn
+
+        from cleaned
+
+    ) ranked
+
+    where rn = 1
+
 )
 
-select *
-from cleaned
+select
+    order_id,
+    customer_id,
+    store_id,
+    order_date,
+    order_status,
+    shipping_fee,
+    currency
+
+from deduped
